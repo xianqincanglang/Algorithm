b9c1893718b942d90ffb67780a6e70396ca15fec
chentianyu <chentianyu@qiyi.com>
2018-05-28 11:37:33 +0800

    Initial Commit

---
+++ Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by chentianyu on 2018/5/28.
+//  Copyright © 2018年 chentianyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
