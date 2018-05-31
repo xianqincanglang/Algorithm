b4c81a0db1891a624b255f5ef7a00ed4f5098b09
chentianyu <chentianyu@qiyi.com>
2018-05-31 15:23:23 +0800

    dev:根据前序、中序，打印链表
    Signed-off-by: chentianyu <chentianyu@qiyi.com>

--- Algorithm/Sword Finger/SwordFinger_1.cpp
+++ Algorithm/Sword Finger/SwordFinger_1.cpp
@@ -129,16 +129,62 @@ vector<int> printListFromTailToHead(ListNode* head) {
     
     return vector;
 }
+#pragma mark - 4
+/**
+ *
+ problem:
+ 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
-void swordFinger_3_test()
-{
-    
-}
+class Solution {
+public:
+    TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
+        int in_size =  vin.size();
+        if (in_size == 0) {
+            return NULL;
+        }
+        
+        vector<int> pre_left,pre_right,in_left,in_right;
+        //创建根节点，根节点肯定是前序遍历的第一个数
+        int rootNodeValue = pre[0];
+        TreeNode *rootNode = new TreeNode(rootNodeValue);
+        
+        //找到中序遍历根节点所在位置,存放于变量p中
+        int p = 0;
+        for (p; p < in_size; ++p) {
+            if (vin[p] == rootNodeValue) {
+                break;
+            }
+        }
+        
+   
+        for(int i=0;i<p;i++){           //构建中序序列的左子树和前序序列的左子树
+            in_left.push_back(vin[i]);
+            pre_left.push_back(pre[i+1]);
+        }
+        for(int i=p+1;i<in_size;i++){   //构建中序序列的右子树和前序序列的右子树
+            in_right.push_back(vin[i]);
+            pre_right.push_back(pre[i]);
+        }
+        
+        //递归
+        rootNode->left = reConstructBinaryTree(pre_left, in_left);
+        rootNode->right = reConstructBinaryTree(pre_right, in_right);
+
+        return rootNode;
+    }
+};
 #pragma mark - test
 void test()
 {
 //    swordFinger_1_test();
 //    swordFinger_2_test();
-    swordFinger_3_test();
+//    swordFinger_3_test();
     
 }
