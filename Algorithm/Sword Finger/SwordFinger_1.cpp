22cabe8330c04129672125c95b0a35f2f83334a3
chentianyu <chentianyu@qiyi.com>
2018-05-31 20:55:27 +0800

    dev:二分查找、递归
    Signed-off-by: chentianyu <chentianyu@qiyi.com>

--- Algorithm/Sword Finger/SwordFinger_1.cpp
+++ Algorithm/Sword Finger/SwordFinger_1.cpp
@@ -9,9 +9,10 @@
 #include "SwordFinger_1.hpp"
 #include <vector>
 #include <iostream>
+#include <stack>
 using namespace std;
 
-#pragma mark - 1
+#pragma mark - 1 二位数组中的查找
 /**
  *
  problem:在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
@@ -48,7 +49,7 @@ void swordFinger_1_test()
     
 }
 
-#pragma mark - 2
+#pragma mark - 2 替换空格
 /**
  *
  problem:替换空格:请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
@@ -92,7 +93,7 @@ void swordFinger_2_test()
     
     cout << c << endl;
 }
-#pragma mark - 3
+#pragma mark - 3 从尾到头打印链表
 /**
  *
  problem:从头到尾打印链表:输入一个链表，从尾到头打印链表每个节点的值。。
@@ -129,7 +130,7 @@ vector<int> printListFromTailToHead(ListNode* head) {
     
     return vector;
 }
-#pragma mark - 4
+#pragma mark - 4 重建二叉树
 /**
  *
  problem:
@@ -180,11 +181,123 @@ public:
         return rootNode;
     }
 };
+#pragma mark - 5 用两个栈实现队列
+/*
+ *用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ */
+stack<int> stack1;
+stack<int> stack2;
+
+//stack1 push
+void push(int node) {
+    stack1.push(node);
+}
+
+//stack2 pop
+int pop() {
+    if (stack2.empty()) {
+        while (!stack1.empty()) {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+    }
+    int value = stack2.top();
+    stack2.pop();
+    return value;
+}
+
+#pragma mark - 6 旋转数组的最小数字
+/*
+ * 知识点:查找
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。 输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ */
+
+
+int minNumberInRotateArray(vector<int> rotateArray) {
+    
+    if (rotateArray.size() == 0) {
+        return 0;
+    }
+    //二分查找
+    int left = 0;
+    int right = rotateArray.size() - 1;
+    int middle = -1;
+    while (rotateArray[left] >= rotateArray[right]) {
+        if (right - left == 1) {
+            middle = right;
+            break;
+        }
+        middle = left+ (right-left)/2;
+        
+        if (rotateArray[middle]>=rotateArray[left]) {
+            left = middle;
+        }else if(rotateArray[right]>=rotateArray[middle]){
+            right = middle;
+        }
+        
+    }
+    
+    
+    return rotateArray[middle];
+}
+#pragma mark - 7 斐波那契数列
+/*
+ *
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。n<=39
+ */
+int Fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n ==1 || n == 2) {
+        return 1;
+    }
+    return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+#pragma mark - 8 跳台阶
+/*
+ *
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+int jumpFloor(int number) {
+    
+    if (number == 1) {
+        return 1;
+    }
+    if (number == 2) {
+        return 2;
+    }
+    return jumpFloor(number - 1)+jumpFloor(number - 2);
+}
+#pragma mark - 9 变态跳台阶
+/*
+ *
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+int jumpFloorII(int number) {
+    
+    /*
+    跳1级，剩下n-1级，则剩下跳法是f(n-1)
+    跳2级，剩下n-2级，则剩下跳法是f(n-2)
+    所以f(n)=f(n-1)+f(n-2)+...+f(1)
+    因为f(n-1)=f(n-2)+f(n-3)+...+f(1)
+    所以f(n)=2*f(n-1)
+     */
+    if (number == 1) {
+        return 1;
+    }
+    if (number == 2) {
+        return 2;
+    }
+    return 2*jumpFloorII(number-1);
+}
 #pragma mark - test
 void test()
 {
 //    swordFinger_1_test();
 //    swordFinger_2_test();
 //    swordFinger_3_test();
-    
+    vector<int> rotateArray = {3,4,5,1,2};
+    cout<< minNumberInRotateArray(rotateArray);
 }
